a980bf38b9a23147bd3323b413be6bb3501482cc
tad dy <a@a.com>
2023-05-14 11:07:21 -0400

    adjust port

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 require("module-alias/register");
 import app from "./server";
 
-const PORT = 4000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);

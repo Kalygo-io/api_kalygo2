d1a99dfc5fb6541c14431d4426d3ffbbdd0c8f2b
tad dy <a@a.com>
2023-05-15 16:28:03 -0400

    temporary hack

--- api/v1/auth/handlers/signUp.ts
+++ api/v1/auth/handlers/signUp.ts
@@ -29,7 +29,8 @@ export async function signUp(req: Request, res: Response, next: NextFunction) {
 
     const emailConfig = generateVerifyEmail_SES_Config(
       email,
-      `${process.env.FRONTEND_HOSTNAME}/verify-email/${emailVerificationToken}`
+      // `${process.env.FRONTEND_HOSTNAME}/verify-email/${emailVerificationToken}`
+      `${process.env.FRONTEND_HOSTNAME}/verify-email/[token].html}`
     );
     await sesClient.send(new SendTemplatedEmailCommand(emailConfig));
 

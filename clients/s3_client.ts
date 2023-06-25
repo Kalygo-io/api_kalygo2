5e175310badf9b0ac6a949204d148f77ac9ab7a6
tad dy <a@a.com>
2023-06-24 22:11:59 -0400

    passing creds to S3Client

--- clients/s3_client.ts
+++ clients/s3_client.ts
@@ -9,6 +9,10 @@ export const s3Client = new S3Client({
 });
 
 export const s3 = new S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
   region: process.env.AWS_REGION,
 });
 

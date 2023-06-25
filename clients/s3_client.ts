5ea1a4245e5fbed2b9537072f2c94e0cc7c98ba7
tad dy <a@a.com>
2023-06-24 22:08:22 -0400

    passing creds to S3Client

--- clients/s3_client.ts
+++ clients/s3_client.ts
@@ -1,6 +1,10 @@
 import { S3, S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 
 export const s3Client = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
   region: process.env.AWS_REGION,
 });
 

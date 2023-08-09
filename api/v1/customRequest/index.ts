2dbd190ed3100dce737411ea0cb2e5abc8f8ce7c
tad dy <a@a.com>
2023-08-09 16:51:52 -0400

    add middleware to customRequest route

--- api/v1/customRequest/index.ts
+++ api/v1/customRequest/index.ts
@@ -2,13 +2,18 @@ import { authenticateToken, multerS3Middleware } from "@/middleware";
 import { customRequest, getCustomRequest, getCustomRequests } from "./handlers";
 
 import { Router } from "express";
+import canCallerPushToQueue from "@/middleware/canCallerPushToQueue";
 
 const router = Router();
 
 router
   .route("/custom-request")
   .post(
-    [authenticateToken, multerS3Middleware.array("documents")],
+    [
+      authenticateToken,
+      canCallerPushToQueue,
+      multerS3Middleware.array("documents"),
+    ],
     customRequest
   );
 

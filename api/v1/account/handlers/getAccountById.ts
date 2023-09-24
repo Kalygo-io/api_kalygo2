c7f774736fdf9b33f8d172c5348df29e8ddf166b
tad dy <a@a.com>
2023-09-23 23:31:40 -0400

    add implementation logic for the getAccountById endpoint guarded by an isAdmin middleware check

--- api/v1/account/handlers/getAccountById.ts
+++ api/v1/account/handlers/getAccountById.ts
@@ -12,8 +12,32 @@ export async function getAccountById(
   try {
     console.log("GET /api/v1/account/:id");
 
+    console.log("req.params.id", req.params.id);
+
+    const account = await prisma.account.findFirst({
+      where: {
+        id: parseInt(req.params?.id),
+      },
+      include: {
+        SummaryCredits: true,
+        VectorSearchCredits: true,
+        CustomRequestCredits: true,
+        UsageCredits: true,
+      },
+    });
+
     // res.status(501).json({});
-    res.status(200).json({});
+    // res.status(200).json(account);
+
+    res.status(200).json({
+      ...pick(account, ["email", "subscriptionPlan", "firstName", "lastName"]),
+      // subscriptions: subscriptions,
+      // stripeDefaultSource: newStripeCustomer.default_source,
+      summaryCredits: get(account, "SummaryCredits.amount", 0),
+      vectorSearchCredits: get(account, "VectorSearchCredits.amount", 0),
+      customRequestCredits: get(account, "CustomRequestCredits.amount", 0),
+      usageCredits: get(account, "UsageCredits.amount", 0),
+    });
   } catch (e) {
     console.log("ERROR in getAccountById", e);
 

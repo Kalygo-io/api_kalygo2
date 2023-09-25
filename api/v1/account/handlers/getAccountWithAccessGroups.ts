dbd04e8d13e9ad8c826f1d556132ea349a211dcc
tad dy <a@a.com>
2023-09-25 18:15:57 -0400

    fixing overly complicated getAccountWithAccessGroups implementation

--- api/v1/account/handlers/getAccountWithAccessGroups.ts
+++ api/v1/account/handlers/getAccountWithAccessGroups.ts
@@ -18,60 +18,24 @@ export async function getAccountWithAccessGroups(
         email: req.user.email,
       },
       include: {
-        CreatedAccessGroups: {
-          include: {
-            accessGroup: true,
-          },
-        },
+        AccessGroups: true,
+      },
+    });
+
+    let publicAccessGroup = await prisma.accessGroup.findFirstOrThrow({
+      where: {
+        // @ts-ignore
+        id: 1,
+        name: "Public",
       },
     });
 
     console.log("testing...");
 
-    // IF the caller is a member of the 'Public' AccessGroup as all accounts should be
-    // THEN return all the callers AccessGroups with the 'Public' one included
-    if (
-      get(account, "CreatedAccessGroups", []).find(
-        (value: any) => value.accessGroupId === 1
-      )
-    ) {
-      res.status(200).json({
-        ...pick(account, ["email", "firstName", "lastName"]),
-        accessGroups: [...get(account, "CreatedAccessGroups", [])],
-      });
-    }
-    // IF the caller is NOT a member of the 'Public' AccessGroup
-    // THEN add them and refetch their account with the AccessGroups they belong to with the 'Public' one now included
-    else {
-      // adding the caller to the 'Public' AccessGroup
-      await prisma.accountsAndAccessGroups.create({
-        data: {
-          accountId: account?.id!,
-          accessGroupId: 1,
-          // @ts-ignore
-          createdById: account?.id!,
-        },
-      });
-      // and fetching caller's account again with all the AccessGroups that are either platform defaults or that they have created (aka own)
-      account = await prisma.account.findFirst({
-        where: {
-          // @ts-ignore
-          email: req.user.email,
-        },
-        include: {
-          CreatedAccessGroups: {
-            include: {
-              accessGroup: true,
-            },
-          },
-        },
-      });
-
-      res.status(200).json({
-        ...pick(account, ["email", "firstName", "lastName"]),
-        accessGroups: [...get(account, "CreatedAccessGroups", [])],
-      });
-    }
+    res.status(200).json({
+      ...pick(account, ["email", "firstName", "lastName"]),
+      accessGroups: [publicAccessGroup, ...get(account, "AccessGroups", [])],
+    });
   } catch (e) {
     console.log("ERROR in getAccountWithAccessGroups", e);
 

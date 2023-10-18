0c7d52c7e5f2bf2e33d4ffbd75072ff862c7f74f
tad dy <a@a.com>
2023-10-18 02:08:49 -0400

    remove unneeded console.log statements

--- jobHandlers/helpers/summaryV2Job/variations/openAi/fileOverall/breakOffMaxChunkForContext.ts
+++ jobHandlers/helpers/summaryV2Job/variations/openAi/fileOverall/breakOffMaxChunkForContext.ts
@@ -9,9 +9,6 @@ export function breakOffMaxChunkForContext(
 ): string[] {
   const tokensInPrefixPlusChunk = (prefix: string, chunk: string) =>
     encoder.encode(`${prefix} ${chunk}`).length;
-
-  p("breaking off max chunk of file..."); // for console debugging...
-
   const dataChunk = chunks[0];
   let endSegmentIndex = dataChunk.length;
   let low = 0;
@@ -21,27 +18,16 @@ export function breakOffMaxChunkForContext(
       dataChunk.substring(0, endSegmentIndex)
     ) !== maxContext
   ) {
-    console.log(
-      "LENGTH",
-      tokensInPrefixPlusChunk(
-        promptPrefix,
-        dataChunk.substring(0, endSegmentIndex)
-      )
-    );
-
     if (
       // prettier-ignore
       tokensInPrefixPlusChunk(promptPrefix, dataChunk.substring(0, endSegmentIndex)) < maxContext
     ) {
-      console.log("IF", "endSegmentIndex", endSegmentIndex);
       low = endSegmentIndex;
       endSegmentIndex = Math.floor((endSegmentIndex + dataChunk.length) / 2);
     } else {
-      console.log("ELSE IF", "endSegmentIndex", endSegmentIndex);
       endSegmentIndex = Math.floor((low + endSegmentIndex) / 2);
     }
   }
-
   const s1 = dataChunk.substring(0, endSegmentIndex);
   const s2 = dataChunk.substring(endSegmentIndex);
   chunks.shift();
